fa0d9ce51d1afffb29cb162440e699e11f629206
= <sadaqsamor99@gmail.com>
2026-08-15 18:37:37 +0100

    path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 
 import Nav from './Components/Nav'
 import Footer from './Components/Footer'
+import { Route, Router, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Products from './Pages/Products'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='mx-20'>
+    <div className='mx-10 md:mx-20'>
      <Nav/>
 
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/products' element={<Products/>}/>
+     </Routes>
 
     <Footer/>
     </div>
