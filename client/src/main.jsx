fd70a7cd050cd8bce5b14fdad4c01699c0e6aaa6
= <sadaqsamor99@gmail.com>
2026-08-15 13:03:01 +0100

    Browser router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import {} from 'react-ro'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
