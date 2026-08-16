4d001212dfe45380c4db7a953a5edb2c601dcf49
= <sadaqsamor99@gmail.com>
2026-08-16 11:14:04 +0100

    loading

---
+++ client/src/Components/SkeletonLoading.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+const SkeletonLoading = () => {
+  return (
+     <div className='w-full  object-cover h-48 rounded-xl  shadow-lg'>
+    <div className='w-full animate-pulse object-cover h-48 rounded-xl bg-gradient from-gray-300 to-100 shadow-lg'>
+                
+    </div>      
+    </div>
+
+  )
+}
+
+export default SkeletonLoading
