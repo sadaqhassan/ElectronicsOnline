803a4bad97e6ee14d29c5cb2f17e2b04be09d80a
= <sadaqsamor99@gmail.com>
2026-08-17 20:30:40 +0100

    NAv

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -4,7 +4,7 @@ import { useApp } from "../context/AppContext"
 
 const Nav = () => {
     const [open, setOpen] =useState(false)
-    const {navigate} = useApp()
+    const {navigate , carts} = useApp()
     return (
         <nav className="flex space-x-10 font-medium items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
@@ -30,7 +30,7 @@ const Nav = () => {
                     <svg width="28" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-green-600 w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-green-600 w-[18px] h-[18px] rounded-full">{carts.length}</button>
                 </div>
 
                 <button className="cursor-pointer px-8 py-2 bg-green-600 hover:bg-green-400 transition text-white rounded-full">
