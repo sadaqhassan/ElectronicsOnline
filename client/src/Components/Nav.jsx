30661df91a8eda24bce67cd41b317cff84c13fac
= <sadaqsamor99@gmail.com>
2026-08-18 13:31:32 +0100

    cart

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import { useApp } from "../context/AppContext"
+import { ShoppingCartIcon } from "lucide-react"
 
 const Nav = () => {
     const [open, setOpen] =useState(false)
     const {navigate , carts} = useApp()
+
     return (
         <nav className="flex space-x-10 font-medium items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
@@ -26,11 +28,10 @@ const Nav = () => {
                     </svg>
                 </div>
 
-                <div className="relative cursor-pointer" onClick={()=>navigate('/carts')}>
-                    <svg width="28" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-green-600 w-[18px] h-[18px] rounded-full">{carts.length}</button>
+                <div className=" relative cursor-pointer " onClick={()=>navigate('/carts')}>
+                   <Link><ShoppingCartIcon/></Link>
+
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-green-600 w-[18px] h-[18px] rounded-full">{carts?.length > 0 ? carts.length : 0 }</button>
                 </div>
 
                 <button className="cursor-pointer px-8 py-2 bg-green-600 hover:bg-green-400 transition text-white rounded-full">
@@ -38,8 +39,15 @@ const Nav = () => {
                 </button>
             </div>
 
+            <div className="relative md:hidden cursor-pointer" onClick={()=>navigate('/carts')}>
+                   <Link><ShoppingCartIcon/></Link>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-green-600 w-[18px] h-[18px] rounded-full">{carts?.length > 0 ? carts.length : 0 }</button>
+                </div>
+
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+
                 {/* Menu Icon SVG */}
+
                 <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect width="21" height="1.5" rx=".75" fill="#426287" />
                     <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#426287" />
@@ -51,12 +59,13 @@ const Nav = () => {
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <Link to={'/'}  className="block">Home</Link>
                 <Link to={'/products'}  className="block">Products</Link>
-                <Link to={'/'}  className="block">Contact</Link>
+                <Link to={'/'} className="block">Contact</Link>
+                
                 <button className="cursor-pointer px-6 py-2 mt-2 bg-green-600 hover:bg-green-400 transition text-white rounded-full text-sm">
                     Login
                 </button>
             </div>
-
+             
         </nav>
     )
 }
