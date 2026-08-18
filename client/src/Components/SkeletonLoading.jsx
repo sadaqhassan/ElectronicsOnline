c16dcabaf571ba0fa6aea3e3ca88dc806d4a4c0f
= <sadaqsamor99@gmail.com>
2026-08-18 15:28:12 +0100

    skeleon

--- client/src/Components/SkeletonLoading.jsx
+++ client/src/Components/SkeletonLoading.jsx
@@ -4,8 +4,8 @@ import ProductCard from './ProductCard'
 const SkeletonLoading = () => {
   return (
      <div className='w-full  object-cover h-48 rounded-xl  shadow-lg'>
-    <div className='w-full animate-pulse object-cover h-48 rounded-xl bg-gradient from-gray-300 to-100 shadow-lg'>
-                
+    <div className='w-full animate-pulse object-cover h-48 rounded-xl bg-gradient from-gray-500 to-300 shadow-lg'>
+              
     </div>      
     </div>
 
