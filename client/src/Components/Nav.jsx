2f4167b73fb42139b6ab72029beccafe727be20d
= <sadaqsamor99@gmail.com>
2026-08-18 14:53:55 +0100

    Nav

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -5,9 +5,10 @@ import { ShoppingCartIcon } from "lucide-react"
 
 const Nav = () => {
     const [open, setOpen] =useState(false)
-    const {navigate , carts} = useApp()
+    const {navigate , carts ,setSearchQuery,searchQuery} = useApp()
 
     return (
+        <div className="flex flex-col space-y-4">
         <nav className="flex space-x-10 font-medium items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
             <div>
@@ -19,14 +20,6 @@ const Nav = () => {
                 <Link to={'/'}>Home</Link>
                 <Link to={'products'} >Products</Link>
                 <Link to={'/contacts'} >Contact</Link>
-
-                <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-                        <path clip-rule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </div>
 
                 <div className=" relative cursor-pointer " onClick={()=>navigate('/carts')}>
                    <Link><ShoppingCartIcon/></Link>
@@ -67,6 +60,14 @@ const Nav = () => {
             </div>
              
         </nav>
+        <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
+                    <input onChange={(e)=>setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path clip-rule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+                </div>
     )
 }
 
